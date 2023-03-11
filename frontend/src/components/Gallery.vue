a7e0cc6a01ad38d22e511a726c490ea48936e4c5
whaji <whaji@etu.u-bordeaux.fr>
2023-03-11 22:56:04 +0100

    l'affichage du message d'erreur dans frontend

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -1,3 +1,13 @@
+<template>
+  <div>
+    <h3>Gallery</h3>
+    <div v-if="imageList.length > 0">
+      <Image v-for="image in imageList" :key="image.id" :id="image.id" />
+    </div>
+    <p v-else-if="error">{{ error }}</p>
+    <p v-else>le dossier images n'existe pas</p>
+  </div>
+</template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import { api } from '@/http-api';
@@ -5,22 +15,17 @@ import { ImageType } from '@/image'
 import Image from './Image.vue';
 
 const imageList = ref<ImageType[]>([]);
+const error = ref<string | null>(null);
 
 api.getImageList()
   .then((data) => {
     imageList.value = data;
   })
   .catch(e => {
-    console.log(e.message);
+    error.value = "Failed to load images. Please try again later.";
+    console.error(e);
   });
 </script>
-
-<template>
-  <div>
-    <h3>Gallery</h3>
-    <Image v-for="image in imageList" :id="image.id" />
-  </div>
-</template>
 
 <style scoped>
 </style>
--- frontend/src/http-api.ts
+++ frontend/src/http-api.ts
@@ -14,7 +14,6 @@ const requests = {
   put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
   delete: (url: string) => instance.delete(url).then(responseBody)
 };
-
 export const api = {
   getImageList: (): Promise<ImageType[]> => {
     return new Promise((resolve, reject) => {
@@ -24,14 +23,20 @@ export const api = {
           if (error.response && error.response.status === 404) {
             instance.get('images/search')
               .then((response) => resolve(responseBody(response)))
-              .catch((error) => reject(error));
+              .catch((error) => {
+                if (error.response && error.response.status === 404) {
+                  reject(new Error("Le dossier 'images' n'a pas été trouvé."));
+                } else {
+                  reject(error);
+                }
+              });
           } else {
             reject(error);
           }
         });
     });
   },
-    getImage: (id: number): Promise<Blob> => requests.get(`images/${id}`, { responseType: "blob" }),
+  getImage: (id: number): Promise<Blob> => requests.get(`images/${id}`, { responseType: "blob" }),
   createImage: (form: FormData): Promise<ImageType> => requests.post('images', form),
   deleteImage: (id: number): Promise<void> => requests.delete(`images/${id}`),
 };
